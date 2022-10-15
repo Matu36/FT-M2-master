f0e43d1b7ac2ec2b219e30a117e6353db476348f
Matu36 <matipineda85@live.com.ar>
2022-10-14 23:27:59 -0300

    REDUX

--- 11-Redux/hw-js/src/index.js
+++ 11-Redux/hw-js/src/index.js
@@ -1,17 +1,18 @@
 const { createStore } = require('redux');
 const contador = require('./reducer');
-const { incremento, decremento } = require('./actions');
+const { incremento, decremento, incrementoImpar, incrementoAsyinc } = require('./actions');
 
 // En esta linea creamos nuestro store. Pasandole como parametro nuestro Reducer
-var store;
+var store = createStore (contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.getElementById ("valor");
 
 // Esta funcion nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
 // Utilizamos el elemento obtenido arriba para mostrar el State.
-function renderContador() {
+function renderContador() { 
+  let num = store.getState().contador;
   // Obtenemos la propiedad 'contador' de nuestro store:
   
   // Seteamos el numero obtenido como texto dentro del elemento con id 'valor':
@@ -20,11 +21,24 @@ function renderContador() {
 
 // Ejecutamos la funcion 'renderContador':
 
-
+renderContador ();
 
 // Nos subscribimos al store pasandole la misma funcion. Asi cada vez que llegue una accion, ejecutamos la funcion:
 
-
+store.subscribe (() => {renderContador ()});
 
 // Por ultimo, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la accion correspondiente:
+
+var btnIncrement = document.getElementbyId ("incremento");
+btnIncrement.onClick = () => {store.dispatch (incremento)};
+
+var btnDecrement = document.getElementbyId ("decremento");
+btnDecrement.onClick = () => {store.dispatch (decremento)};
+
+var btnImpar = document.getElementById ("incrementoImpar");
+btnImpar.onClick = () => {store.dispatch (incrementoImpar())};
+
+var btnAsync = document.getElementbyId ("incrementoAsyinc");
+btnAsync.onClick = () => {setTimeout (() => { store.dispatch (incrementoAsyinc()) }, 1000) };
+
